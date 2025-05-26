21378c71d66f8b3be173740306f68116267fb15a
Randy340 <randy340@gmail.com>
2025-05-25 22:22:19 -0500

    fixed os.makedirs error in prep file

--- data-science/src/prep.py
+++ data-science/src/prep.py
@@ -38,8 +38,8 @@ def main(args):  # Write the function name for the main data preparation logic
     train_df, test_df = train_test_split(df, test_size=args.test_train_ratio, random_state=42)  #  Write code to split the data into train and test datasets
 
     # Save the train and test data
-    os.makedirs(args.train_data, exist_ok=True)  # Create directories for train_data and test_data
-    os.makedirs(args.test_data, exist_ok=True)  # Create directories for train_data and test_data
+    # os.makedirs(args.train_data, exist_ok=True)  # Create directories for train_data and test_data
+    # os.makedirs(args.test_data, exist_ok=True)  # Create directories for train_data and test_data
     train_df.to_csv(os.path.join(args.train_data, "train.csv"), index=False)  # Specify the name of the train data file
     test_df.to_csv(os.path.join(args.test_data, "test.csv"), index=False)  # Specify the name of the test data file
 
